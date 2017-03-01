04d271ca2c524b36602ccdd411518d094f78a9c9
Luis Carlin <luiscarlin@gmail.com>
2017-03-01 07:11:46 -0500

    a game cannot start without players

--- src/main/java/com/tennisgame/TennisGame.java
+++ src/main/java/com/tennisgame/TennisGame.java
@@ -4,4 +4,8 @@ public class TennisGame {
     public String getState() {
         return "Waiting for players";
     }
+
+    public void start() throws IllegalStateException {
+        throw new IllegalStateException("The game cannot start without players");
+    }
 }
--- src/test/java/com/tennisgame/TennisGameTest.java
+++ src/test/java/com/tennisgame/TennisGameTest.java
@@ -1,15 +1,36 @@
 package com.tennisgame;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TennisGameTest {
+
+    TennisGame underTest;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        underTest = new TennisGame();
+    }
+
     @Test
     public void aGameWaitsForPlayersToStart() throws Exception {
-        TennisGame underTest = new TennisGame();
         String state = underTest.getState();
         assertThat(state, is("Waiting for players"));
+    }
+
+    @Test
+    public void aGameShouldNotStartWithoutPlayers() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage(containsString("cannot start without players"));
+        underTest.start();
     }
 }
