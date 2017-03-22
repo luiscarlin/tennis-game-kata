4416f21f0126f66bf6b52422d3734a92799812c7
Luis Carlin <luiscarlin@gmail.com>
2017-03-22 08:15:04 -0400

    Refactor how players are stored

--- src/main/java/com/tennisgame/TennisGame.java
+++ src/main/java/com/tennisgame/TennisGame.java
@@ -1,9 +1,6 @@
 package com.tennisgame;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TennisGame {
 
@@ -24,12 +21,14 @@ public class TennisGame {
 
     }
     private final List<String> DESCRIPTIONS = Arrays.asList("love", "fifteen", "thirty", "forty");
-    private Map<Player, Integer> players;
+    private Map<Player, Integer> scores;
+    private List<Player> players;
 
     private State state;
 
     TennisGame() {
-        players = new HashMap<Player, Integer>();
+        scores = new HashMap<Player, Integer>();
+        players = new ArrayList<Player>();
         state = State.WAITING_FOR_PLAYERS;
     }
 
@@ -39,32 +38,35 @@ public class TennisGame {
 
     public void start() throws IllegalStateException {
         if (state == State.READY_TO_START) {
+            scores.put(players.get(0), 0);
+            scores.put(players.get(1), 0);
+
             state  = State.GAME_STARTED;
         }
         else {
-            throw new IllegalStateException("The game cannot start without two players");
+            throw new IllegalStateException("The game cannot start without two scores");
         }
     }
 
     public void addPlayer(Player player) {
-        players.put(player, 0);
+        players.add(player);
 
-        if(players.size() == 2) {
+        if (players.size() == 2) {
             state = State.READY_TO_START;
         }
     }
 
     public Integer getScore(Player player) {
-        return players.get(player);
+        return scores.get(player);
     }
 
     public String getScoreDescription(Player player) {
-        int score = players.get(player);
+        int score = scores.get(player);
         return DESCRIPTIONS.get(score);
     }
 
     public void winBall(Player player) {
-        int score = players.get(player);
-        players.put(player, score + 1);
+        int score = scores.get(player);
+        scores.put(player, score + 1);
     }
 }
